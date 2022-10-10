f24526202990c56151b2effb90e0e71a6eaced32
zi hui <zihuic02@gmail.com>
2022-10-10 16:45:33 +0800

    randomise listing designs

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -6,6 +6,35 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import {useNavigate} from "react-router-dom";
+import {useState, useEffect} from "react";
+
+import placeholder from "../assets/image_placeholder.png";
+import bgrnd1 from "../assets/backgroundpic1.jpg";
+import bgrnd2 from "../assets/backgroundpic2.jpg";
+import bgrnd3 from "../assets/backgroundpic3.jpg";
+import bgrnd4 from "../assets/backgroundpic4.jpg";
+import bgrnd5 from "../assets/backgroundpic5.jpg";
+import bgrnd6 from "../assets/backgroundpic6.jpg";
+import bgrnd7 from "../assets/backgroundpic7.jpg";
+import bgrnd8 from "../assets/backgroundpic8.jpg";
+
+const bgrnds = [
+  bgrnd1, bgrnd2, bgrnd3, bgrnd4, bgrnd5, bgrnd6, bgrnd7, bgrnd8
+]
+
+function RandomBackground() {
+  const [currentBgrndIndex, setCurrentBgrndIndex] = useState(Math.floor(Math.random() * bgrnds.length))
+  const changeBgrnd = () => {
+    const randomNumber = Math.floor(Math.random()*bgrnds.length);
+    setCurrentBgrndIndex(randomNumber);
+    console.log(Math.random());
+  }
+  useEffect(() => changeBgrnd(), [])
+
+  return (
+    bgrnds[currentBgrndIndex]
+  )
+}
 
 export default function Listing({name, description}) {
   const navigate = useNavigate();
@@ -14,7 +43,7 @@ export default function Listing({name, description}) {
       <CardMedia
         component="img"
         height="140"
-        image="../assets/image_placeholder.png"
+        image={RandomBackground()}
         alt="image placeholder"
       />
       <CardContent sx={{ textAlign: "left" }}>
