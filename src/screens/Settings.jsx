d931400b888a82b2d02a1636d71a97a60b6c6249
AdH01C <adrianhengyl@gmail.com>
2022-11-02 13:02:33 +0800

    Profile update works and verification added

--- src/screens/Settings.jsx
+++ src/screens/Settings.jsx
@@ -26,6 +26,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import validator from "validator";
 
 import NavigationBar from "../components/NavigationBar.jsx";
+import isStrongPassword from "validator/es/lib/isStrongPassword.js";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -103,42 +104,76 @@ export default function Settings() {
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => { setValue(newValue); };
 
-  const putApplication = async (userId, listingId) => {
-    const IpAddresss = 0;
-    const res = await axios.put("/user/update/name/${userId}", {
-      data: { firstname: "firstname", lastname: "lastname" },
-      params: { userId: userId },
-      auth: { username: "admin@lendahand.com", password: "password" }
-    })
-  };
-  const handleNameChangePasswordSubmit = (event) => {
+  const handleNameChangeProfileSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const input = {
-      "newpassword": data.get('newpassword')
-    }
 
-    if (JSON.stringify(input.newpassword).length === 0) {
-      console.log("some fields are empty!");
-      handleBlankOpen();
+    const input =
+        {
+          "firstname": data.get('firstName'),
+          "lastname": data.get('lastName'),
+          "contactNo": data.get('phoneNo'),
+        }
+
+    if (input.firstname.length === 0 | input.lastname.length === 0 | input.contactNo.length !== 8 | isNaN(+input.contactNo) ) {
+      console.log("some fields are invalid!");
+
     } else {
-      axios.post(
-          'http://localhost:8080/resetting/password/' + localStorage.getItem("userid"),
+      axios.put('http://localhost:8080/user/reseting/profile/' + localStorage.getItem("userid"),
+          {
+            "firstname": input.firstname,
+            "lastname": input.lastname,
+            "contact": input.contactNo,
+          },
           {auth:
                 {
-                  "username": localStorage.getItem("username"),
-                  "password": localStorage.getItem("password"),
-                }
-          })
+                  username: localStorage.getItem("username"),
+                  password: localStorage.getItem("password")
+                }},
+          {headers: {
+              'Access-Control-Allow-Origin': 'http://localhost:8080',
+              'Content-Type': 'application/json',
+            }})
           .then((response) => {
-            console.log("successful update!");
-            handleLogInOpen();
-
+            console.log(response);
+            localStorage.setItem("firstname", response.data.firstname)
+            localStorage.setItem("lastname", response.data.lastname)
+            window.location.reload(false);
           }, (error) => {
-            console.log("unsuccessful update");
-            handleFailedOpen();
+            console.log(error);
           });
     }
+  };
+
+  const handleNameChangePasswordSubmit = (event) => {
+    const formData = new FormData(event.currentTarget)
+    if (data.get("newpassword").length < 8 || data.get("newpassword").length !== data.get("confirmpassword").length || !isStrongPassword(data.get("newpassword"))) {
+      console.log("some fields are invalid!");
+      // handleBlankOpen();
+
+    } else {
+      axios.put('http://localhost:8080/user/resetting/password/' + localStorage.getItem("userid"),
+          {
+            "password": data.get('password'),
+          },
+          {auth:
+                {
+                  username: localStorage.getItem("username"),
+                  password: localStorage.getItem("password")
+                }},
+          {headers: {
+              'Access-Control-Allow-Origin': 'http://localhost:8080',
+              'Content-Type': 'application/json',
+            }})
+          .then((response) => {
+            console.log(response);
+            localStorage.setItem("password", response.data.password)
+            window.location.reload(false);
+          }, (error) => {
+            console.log(error);
+          });
+    }
+
   }
 
   //code to handle opening and closing of the confirmation pop-up
@@ -170,22 +205,10 @@ export default function Settings() {
   function CheckIfNumber() {
     var input = event.target.value;
     // useEffect(() => {
-    if (isNaN(input) || !(input[0] == 8 || input[0] == 6 || input[0] == 9)) {
+    if (isNaN(input) || !(input[0] === 8 || input[0] === 6 || input[0] === 9)) {
       setPhoneErrorState(true);
     } else {
       setPhoneErrorState(false);
-    }
-  }
-
-  //code for input validation of email
-  const [emailErrorState, setEmailErrorState] = useState(false);
-  const emailErrorMessage = "Enter a valid email."
-  const validateEmail = (e) => {
-    var email = e.target.value;
-    if (validator.isEmail(email)) {
-      setEmailErrorState(false);
-    } else {
-      setEmailErrorState(true);
     }
   }
 
@@ -194,7 +217,7 @@ export default function Settings() {
   const [password, setPassword] = useState("");
   const passwordErrorMessage = "The password does not match!"
   const validatePassword = (e) => {
-    if (password == e.target.value) {
+    if (password === e.target.value) {
       setPasswordNotMatch(false);
     } else {
       setPasswordNotMatch(true);
@@ -202,97 +225,122 @@ export default function Settings() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth={false} disableGutters sx={{ background: "white", direction: "row", justifyContent: "flex-start", width: "100vw", display: "flex", flexDirection: "row" }}>
-        <CssBaseline />
-        <Box disableGutters sx={{ background: "white", }}>
-          <NavigationBar />
-        </Box>
-        <Container disableGutters sx={{ background: "white", direction: "column", justifyContent: "flex-start", width: "20vw", height: '80vh'}}>
-          <Tabs orientation="vertical" variant="scrollable" value={value} onChange={handleChange} sx={{ borderRight: 1, borderColor: 'divider' }}>
-            <Tab label="Profile" {...a11yProps(0)} />
-            <Tab label="Privacy" {...a11yProps(1)} />
-            <Tab label="Preferences" {...a11yProps(2)} disabled />
-            <Tab label="Report" {...a11yProps(3)} disabled />
-            <br ></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <Tab label="About" {...a11yProps(4)} disabled />
-          </Tabs>
-        </Container>
-        {/*<Container maxWidth={false} disableGutters sx={{ background: "white", direction: "column", justifyContent: "center", width: "100vw", }}>*/}
+      <ThemeProvider theme={theme}>
+        <Container maxWidth={false} disablegutters sx={{
+          background: "white",
+          direction: "row",
+          justifyContent: "flex-start",
+          width: "100vw",
+          display: "flex",
+          flexDirection: "row"
+        }}>
+          <CssBaseline/>
+          <Box disablegutters sx={{background: "white",}}>
+            <NavigationBar/>
+          </Box>
+          <Container disableGutters sx={{
+            background: "white",
+            direction: "column",
+            justifyContent: "flex-start",
+            width: "20vw",
+            height: '80vh'
+          }}>
+            <Tabs orientation="vertical" variant="scrollable" value={value} onChange={handleChange}
+                  sx={{borderRight: 1, borderColor: 'divider'}}>
+              <Tab label="Profile" {...a11yProps(0)} />
+              <Tab label="Privacy" {...a11yProps(1)} />
+              <Tab label="Preferences" {...a11yProps(2)} disabled/>
+              <Tab label="Report" {...a11yProps(3)} disabled/>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <Tab label="About" {...a11yProps(4)} disabled/>
+            </Tabs>
+          </Container>
+          {/*<Container maxWidth={false} disableGutters sx={{ background: "white", direction: "column", justifyContent: "center", width: "100vw", }}>*/}
           {/*<Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', }}>*/}
-            <Container disableGutters sx={{ background: "white" }}>
-              <TabPanel value={value} index={0}>
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 2 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{ width: 100, height: 100 }} />
-                </IconButton>
-                <Typography component="h1" variant="h5"> Welcome back, {localStorage.getItem('firstname')}. </Typography>
+          <Container disablegutters sx={{background: "white"}}>
+            <TabPanel value={value} index={0}>
+              <IconButton onClick={handleOpenUserMenu} sx={{p: 2}}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{width: 100, height: 100}}/>
+              </IconButton>
+              <Typography component="h1" variant="h5"> Welcome back, {localStorage.getItem('firstname')}. </Typography>
 
-                <Typography variant="body2" sx={{ p: 2 }}> Edit your info and settings to make Lend a Hand work better for you. <a href='#'>Learn more </a></Typography>
+              <Typography variant="body2" sx={{p: 2}}> Edit your info and settings to make Lend a Hand work better for
+                you. <a href='#'>Learn more </a></Typography>
 
-                <Box component="form" noValidate onSubmit={handleNameChangePasswordSubmit} sx={{ mt: 3 }}>
-                  <Grid container spacing={5} direction="column" justifyContent="center" alignItems="stretch" >
-                    <Grid item spacing={2}>
-                      <TextField autoComplete="given-name" name="firstName" required fullWidth id="firstName" label="First Name" inputProps={{ maxLength: 100 }} autoFocus />
-                      <TextField required fullWidth id="lastName" label="Last Name" name="lastName" autoComplete="family-name" inputProps={{ maxLength: 100 }} />
-                      <TextField required fullWidth id="phoneNo" label="Phone Number" name="phoneNo" autoComplete="phone number" type="tel" inputProps={{ maxLength: 8 }} onChange={CheckIfNumber} error={phoneErrorState} />
-                    </Grid>
-                    <Grid item xs zeroMinWidth>
-                      <Grid item xs={12} sm container>
-                        <Grid item xs container direction="left" spacing={2}> </Grid>
-                        <Grid item>
-                          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}> Save Changes </Button>
-                        </Grid>
+              <Box component="form" noValidate onSubmit={handleNameChangeProfileSubmit} sx={{mt: 3}}>
+                <Grid container spacing={5} direction="column" justifyContent="center" alignItems="stretch">
+                  <Grid item spacing={2}>
+                    <TextField autoComplete="given-name" name="firstName" required fullWidth id="firstName"
+                               label="First Name" inputProps={{maxLength: 100}} autoFocus/>
+                    <TextField required fullWidth id="lastName" label="Last Name" name="lastName"
+                               autoComplete="family-name" inputProps={{maxLength: 100}}/>
+                    <TextField required fullWidth id="phoneNo" label="Phone Number" name="phoneNo"
+                               autoComplete="phone number" type="tel" inputProps={{maxLength: 8}}
+                               onChange={CheckIfNumber} error={phoneErrorState}/>
+                  </Grid>
+                  <Grid item xs zeroMinWidth>
+                    <Grid item xs={12} sm container>
+                      <Grid item xs container direction="left" spacing={2}> </Grid>
+                      <Grid item>
+                        <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}> Save Changes </Button>
                       </Grid>
                     </Grid>
                   </Grid>
-                </Box>
-              </TabPanel>
+                </Grid>
+              </Box>
+            </TabPanel>
 
-              <TabPanel value={value} index={1}>
-                <Typography component="h1" variant="h5" sx={{ p: 2 }}> Change your password </Typography>
-                <Typography variant="body2" sx={{p: 1}}> Choose a strong password and don't reuse it for other accounts. <a href='#'>Learn more </a></Typography>
-                <Box component="form" noValidate onSubmit={handleNameChangePasswordSubmit} sx={{ mt: 3 }}>
-                  <Grid container spacing={5} direction="column" justifyContent="center" alignItems="stretch">
-                    <Grid item spacing={2}>
-                      <TextField required fullWidth name="password" label="Current Password" type="password" id="password" autoComplete="new-password" inputProps={{ maxLength: 100 }} onChange={(e) => setPassword(e.target.value)} />
-                      <TextField required fullWidth name="password" label="New Password" type="password" id="password" autoComplete="new-password" inputProps={{ maxLength: 100 }} onChange={(e) => setPassword(e.target.value)} />
-                      <TextField required fullWidth name="confirmpassword" label="Confirm Password" type="password" id="confirmpassword" autoComplete="confirm-password" inputProps={{ maxLength: 100 }} helperText={passwordNotMatch && passwordErrorMessage} onChange={(e) => validatePassword(e)} error={passwordNotMatch} />
-                    </Grid>
-                    <Grid item xs zeroMinWidth>
-                      <Grid item xs={12} sm container>
-                        <Grid item xs container direction="left" spacing={2}> </Grid>
-                        <Grid item>
-                          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}> Save Changes </Button>
-                        </Grid>
+            <TabPanel value={value} index={1}>
+              <Typography component="h1" variant="h5" sx={{p: 2}}> Change your password </Typography>
+              <Typography variant="body2" sx={{p: 1}}> Choose a strong password and don't reuse it for other
+                accounts. <a href='#'>Learn more </a></Typography>
+              <Box component="form" noValidate onSubmit={handleNameChangePasswordSubmit} sx={{mt: 3}}>
+                <Grid container spacing={5} direction="column" justifyContent="center" alignItems="stretch">
+                  <Grid item spacing={2}>
+                    <TextField required fullWidth name="password" label="New Password" type="password" id="password"
+                               autoComplete="new-password" inputProps={{maxLength: 100}}
+                               onChange={(e) => setPassword(e.target.value)}/>
+                    <TextField required fullWidth name="confirmpassword" label="Confirm Password" type="password"
+                               id="confirmpassword" autoComplete="confirm-password" inputProps={{maxLength: 100}}
+                               helperText={passwordNotMatch && passwordErrorMessage}
+                               onChange={(e) => validatePassword(e)} error={passwordNotMatch}/>
+                  </Grid>
+                  <Grid item xs zeroMinWidth>
+                    <Grid item xs={12} sm container>
+                      <Grid item xs container direction="left" spacing={2}> </Grid>
+                      <Grid item>
+                        <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}> Save Changes </Button>
                       </Grid>
                     </Grid>
                   </Grid>
-                </Box>
-              </TabPanel>
+                </Grid>
+              </Box>
+            </TabPanel>
 
-              <TabPanel value={value} index={2}>
-                Item Three
-              </TabPanel>
-            </Container>
+            <TabPanel value={value} index={2}>
+              Item Three
+            </TabPanel>
+          </Container>
           {/*</Box>*/}
-        {/*</Container>*/}
-      </Container>
-    </ThemeProvider>);
+          {/*</Container>*/}
+        </Container>
+        <Copyright/>
+      </ThemeProvider>);
 }
 
 //
