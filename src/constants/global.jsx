b2e1a74528dc9ec2adbf2a4358c2ee2b6d0df7b8
zi hui <zihuic02@gmail.com>
2022-11-08 00:47:33 +0800

    test proxy server

--- src/constants/global.jsx
+++ src/constants/global.jsx
@@ -1,2 +1,3 @@
-const url = "https://cors-anywhere.herokuapp.com/54.95.245.238:8080"
+//const url = "https://cors-anywhere.herokuapp.com/54.95.245.238:8080"
+const url = "https://cs203-frontend.herokuapp.com//54.95.245.238:8080"
 export default url;
