68e976f37c447693de4e38faac6e8b9b3217945f
zi hui <zihuic02@gmail.com>
2022-11-08 11:29:04 +0800

    pls deploy

--- src/constants/global.jsx
+++ src/constants/global.jsx
@@ -1,3 +1,3 @@
 //const url = "https://cors-anywhere.herokuapp.com/54.95.245.238:8080"
-const url = "https://cs203-react-frontend.herokuapp.com/54.95.245.238:8080"
+const url = "https://cs203-react-frontend.herokuapp.com/api.lendahand.life:8080"
 export default url;
