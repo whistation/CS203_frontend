f31357d8ac1cc5ceaf16910a6b63af684bca3c5e
zi hui <zihuic02@gmail.com>
2022-11-12 16:17:43 +0800

    final url update

--- src/constants/global.jsx
+++ src/constants/global.jsx
@@ -1,4 +1,2 @@
-//const url = "https://cors-anywhere.herokuapp.com/54.95.245.238:8080"
-const url = "https://cs203-react-frontend.herokuapp.com/13.114.159.137:8080"
-//const url = "http://localhost:8080"
+const url = "api.lendahand.life"
 export default url;
